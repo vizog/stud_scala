900e5b930293e59f0aca1b1b36267746b7b63ad3
Vahid Zoghi <vahid.zoghi@gmail.com>
2011-07-05 00:59:09 +0430

    added student actor learned about react and receive

--- src/domain/Student.scala
+++ src/domain/Student.scala
@@ -1,5 +1,27 @@
 package domain
 
-class Student {
+import scala.actors.Actor;
+
+//messages:
+
+case object SayFirstName
+case object SayLastName
+
+class Student extends Actor {
+  var fName = "ali"
+  var lName = "gholi"
+
+  def act() {
+    loop {
+      react {
+        case SayFirstName =>
+          println(fName)
+        case SayLastName =>
+          println(lName)
+        case exit =>
+          exit
+      }
+    }
+  }
 
 }
--- test/test/domain/testDomain.scala
+++ test/test/domain/testDomain.scala
@@ -1,7 +1,16 @@
 package test.domain
 
+import domain._
 object testDomain {
   def main(args : Array[String]) : Unit = {
     println("hello I am Working");
+    
+    var st = new Student
+    st.start
+    st ! SayFirstName
+    st ! SayLastName
+    st ! "boomBang"
+    st ! exit
+    st ! "gholaam"
   }
 }
