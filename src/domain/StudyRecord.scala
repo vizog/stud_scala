5136639bfba4ce666c472bcf0641fdd8b3f3074b
Vahid Zoghi <vahid.zoghi@gmail.com>
2012-05-05 16:14:17 +0430

    minor change

--- src/domain/StudyRecord.scala
+++ src/domain/StudyRecord.scala
@@ -22,19 +22,6 @@ class StudyRecord(
             offering ! HasPassed(course, target)
             debug("[StudyRec:" + this + "] sent: HasPassed2(" + course + ", " + target + ") + to " + offering);
           }
-        //        case AreYouAPassCourseRecord(course, target) =>
-        //          debug("[StudyRec:" + this + "] received: AreYouAPassCourseRecord("+course+", "+target+")");
-        //          if (grade < 10) {
-        //            debug("[StudyRec:" + this + "] sent: Passed(" + course + ", false) to " + target);
-        //            target !Passed(course, false);
-        ////           	reply(Passed(course, false));
-        //          }
-        //          else {
-        //            val res = offering !? IsYourCourse(course, self)
-        //            debug("[StudyRec:" + this + "] sent: Passed(" + course + ", " + res + ") + to " + target);
-        //            target ! Passed(course, res.asInstanceOf[Boolean])
-        ////            reply(Passed(course, res.asInstanceOf[Boolean]))
-        //          }
         case exit =>
           exit
       }
